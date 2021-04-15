00dfc1233d53f6f996643fd2599035c959ca3e8a
Fercasanova8704 <82071059+Fercasanova8704@users.noreply.github.com>
2021-04-15 02:39:20 -0300

    ´mensaje´

---
+++ Formulario .cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPFINAL_CASANOVA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form Formulario = new Pacientes();
+            Formulario.Show();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form Formulario2 = new Turnos();
+            Formulario2.Show();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form Formulario3 = new Medicos();
+            Formulario3.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form Formulario4 = new Recepcionistas();
+            Formulario4.Show();
+
+            
+
+        }
+    }
+}
---
+++ Recepcionistas.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPFINAL_CASANOVA
+{
+    public partial class Recepcionistas : Form
+    {
+        public Recepcionistas()
+        {
+            InitializeComponent();
+        }
+         
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+
+            if (textBox1.Text == "" || textBox1.Text == " ")
+                 MessageBox.Show("Debe completar el campo");
+            if (textBox2.Text == "" || textBox2.Text == " ")
+                MessageBox.Show("Debe completar el campo");
+            if (textBox3.Text == "" || textBox3.Text == " ")
+                MessageBox.Show("Debe completar el campo");
+            if (textBox4.Text == "" || textBox4.Text == " ")
+                MessageBox.Show("Debe completar el campo");
+            if (textBox5.Text == "" || textBox5.Text == " ")
+                MessageBox.Show("Debe completar el campo");
+
+            else
+            {
+                MessageBox.Show("La alta a sido dada exitosamente");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "  ";
+            textBox2.Text = "  ";
+            textBox3.Text = "  ";
+            textBox4.Text = "  ";
+            textBox5.Text = "  ";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Recepcionistas_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
